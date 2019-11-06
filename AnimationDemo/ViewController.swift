460499f360885a7d5f7399d4aa2f5995d907ee21
Carter Shoemaker <actuallycarter@gmail.com>
2019-11-06 12:11:15 -0500

    spin

--- AnimationDemo/ViewController.swift
+++ AnimationDemo/ViewController.swift
@@ -21,6 +21,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func animationSpin(_ sender: UIButton) {
+        
+        let rotation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = Double.pi * 2
+        rotation.duration = 2.0 // or however long you want ...
+        rotation.isCumulative = true
+        rotation.repeatCount = 0
+        viewToAnimate.layer.add(rotation, forKey: "rotationAnimation")
+        
     }
     
     @IBAction func animationMove(_ sender: UIButton) {
